4b0b12666dccf48745a0d329e806fb2959a9a750
Dmytro Sapozhnyk <dimonsuper010@gmail.com>
2019-03-07 20:27:55 +0200

    Fixed styles, added variables.

--- kvadrat.js
+++ kvadrat.js
@@ -1,17 +1,34 @@
-function create() {
-	for(var i = 0; i < 2000; i++) {
-			createUserSegment();
+var game = {
+	size: '20x10',
+	
+};
+
+
+window.onload = function () {
+	render();
+};
+
+function render() {
+	var size = game.size.split('x');
+	var height = +size[1], width = +size[0];
+	
+	for (var i = 0; i < width; i++) {
+		for (var j = 0; j < height; j++) {
+			createUserSegment()
 		}
-}		
+	}
+}
 
 
 function createUserSegment() {
-  	var div = document.createElement("div");
-  	div.className = 'square';
-  	div.onclick = function() {
-  	div.style.backgroundColor = "blue";
-  	};
-  	document.getElementById("field").appendChild(div);
+	var div = document.createElement("div");
+	div.className = 'square';
+	div.onclick = function () {
+		div.style.backgroundColor = "blue";
+	};
+	document.getElementById("field").appendChild(div);
 }
 
-create();
+function handleChangeSize(event) {
+	game.size = event.target.value;
+}
