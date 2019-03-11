3d6ddca2ec6fa250aedce44090b08a079c3ba9bd
Mykola Shwayko <kolik2001@gmail.com>
2019-03-11 18:33:20 +0200

    Trying make render

--- kvadrat.js
+++ kvadrat.js
@@ -1,17 +1,62 @@
-function create() {
-	for(var i = 0; i < 2000; i++) {
-			createUserSegment();
+var field = document.getElementById('field');
+
+var game = {
+	size: '20x10',
+	
+};
+
+
+window.onload = function () {
+	render();
+};
+
+function render() {
+	var size = game.size.split('x');
+	var height = +size[1], width = +size[0];
+	
+	for (var i = 0; i < width; i++) {
+		for (var j = 0; j < height; j++) {
+
+			createUserSegment()
+		
 		}
-}		
+	}
+}
 
 
 function createUserSegment() {
-  	var div = document.createElement("div");
-  	div.className = 'square';
-  	div.onclick = function() {
-  	div.style.backgroundColor = "blue";
-  	};
-  	document.getElementById("field").appendChild(div);
+	var div = document.createElement("div");
+	div.className = 'square';
+	div.onclick = function () {
+		div.style.backgroundColor = "blue";
+	};
+	document.getElementById("field").appendChild(div);
 }
 
-create();
+function deleteField(){
+	getElementByClassName('square');
+	for (var i = 0; i < width * height; i++) {
+			field.removeElement(square);
+		}
+		render();
+	}
+
+function destr() { 
+	debugger
+	deleteField();
+}
+
+function fieldSize(fieldsize) {
+	if (game.size == "20x10"){
+		document.getElementById(field).style.width = '300px';
+	}
+	if (game.size == "50x40"){
+		document.getElementById(field).style.width = '700px';
+	}
+}
+
+function handleChangeSize(event) {
+	game.size = event.target.value;
+	fieldSize(game.size);
+	render();
+}
