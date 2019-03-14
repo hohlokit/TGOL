c87cc57dfd42efda98ec20c5584e440b76d375fc
Mykola Shwayko <kolik2001@gmail.com>
2019-03-14 20:34:18 +0200

    Clear

--- kvadrat.js
+++ kvadrat.js
@@ -2,9 +2,26 @@ var field = document.getElementById('field');
 
 var game = {
 	size: '20x10',
-
+	arr: []
 };
 
+function handleSquare(event) {
+
+	var id = event.target.id;
+
+	if (id != 'field') {
+		if (event.target.style.backgroundColor === "") {
+			event.target.style.backgroundColor = "blue";
+			game.arr[id].selected = true;
+		}
+		else {
+			event.target.style.backgroundColor = "";
+			game.arr[id].selected = false;
+		}
+	}
+	else
+	debugger;
+}
 
 window.onload = function () {
 	render();
@@ -13,22 +30,21 @@ window.onload = function () {
 function render() {
 	var size = game.size.split('x');
 	var height = +size[1], width = +size[0];
-
+	var n = 0;
 	for (var i = 0; i < width; i++) {
 		for (var j = 0; j < height; j++) {
-			createUserSegment()
-
+			createUserSegment(n);
+			game.arr.push({ selected: false });
+			n++;
 		}
 	}
 }
 
 
-function createUserSegment() {
+function createUserSegment(sum) {
 	var div = document.createElement("div");
 	div.className = 'square';
-	div.onclick = function () {
-		div.style.backgroundColor = "blue";
-	};
+	div.id = sum;
 	document.getElementById("field").appendChild(div);
 }
 
@@ -41,17 +57,17 @@ function clearField() {
 
 
 function fieldSize(fieldsize) {
-	if (game.size == "20x10") {
+	if (game.size === "20x10") {
 		var temp = document.getElementById('field');
 		temp.style.height = '150px';
 		temp.style.width = '300px';
 	}
-	if (game.size == "50x30") {
+	if (game.size === "50x30") {
 		var temp = document.getElementById("field");
 		temp.style.width = '750px';
 		temp.style.height = '450px';
 	}
-	if (game.size == "70x50") {
+	if (game.size === "70x50") {
 		var temp = document.getElementById('field');
 		temp.style.height = '750px';
 		temp.style.width = '1050px';
